a551f840a91196b137d52b36edd795887521585c
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-08 21:28:18 +0800

    setup socket.io on the server

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const PORT = process.env.PORT || 3030;
 
 const app = express();
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'public/index.html')); 
+});
+
+io.on('connection', (socket) => {
+  console.log('user connected to Socket.IO server', socket.id);
 });
 
 server.listen(PORT, () => {
