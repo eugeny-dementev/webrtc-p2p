ff432c6332e976b758524ab2d9739c3ff11ec89b
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-08 21:40:08 +0800

    feat: track connected socket.io users

--- server.js
+++ server.js
@@ -11,13 +11,23 @@ const io = require('socket.io')(server);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'public/index.html')); 
+  res.sendFile(path.join(process.cwd(), 'public/index.html'));
 });
 
+const connectedPeers = new Set();
+
 io.on('connection', (socket) => {
+  connectedPeers.add(socket.id);
   console.log('user connected to Socket.IO server', socket.id);
+  console.log('connectedPeers', connectedPeers);
+
+  socket.on('disconnect', () => {
+    connectedPeers.delete(socket.id);
+    console.log('user disconnected to Socket.IO server', socket.id);
+    console.log('connectedPeers', connectedPeers);
+  });
 });
 
 server.listen(PORT, () => {
-  console.log('Server is strated on', PORT);
+  console.log('Server is started on', PORT);
 });
