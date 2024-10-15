d4b1f45b1dbb5733ca0ab2a37ff49a57cc7d3d35
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-15 23:11:53 +0800

    feat: prepare preOfferHandler logic

--- public/webRTCHandler.js
+++ public/webRTCHandler.js
@@ -1,6 +1,7 @@
 import { assert } from "./assert.js";
 import { CALL_TYPE } from "./constants.js";
 import * as wss from './wss.js';
+import * as ui from './ui.js';
 
 export function sendPreOffer(code, type) {
   assert.isString(code, 'offer code should be a string');
@@ -25,5 +26,23 @@ export function handlePreOffer(data) {
   assert.oneOf(data.callType, Object.values(CALL_TYPE));
   assert.isString(data.callerSocketId, 'data.callerSocketId should be a string');
 
+  const { callType, callerSocketId } = data;
+
+  const connectedUserDetails = {
+    socketId: callerSocketId,
+    callType,
+  };
+
+  if (callType === CALL_TYPE.PersonalChat || callType === CALL_TYPE.PersonalCall) {
+    ui.showCallingDialog(callType, acceptCallHandler, rejectCallHandler);
+  }
+
   console.log('Callee received preOffer', data);
+}
+
+function acceptCallHandler() { 
+  console.log('acceptCallHandler()');
+}
+function rejectCallHandler() {
+  console.log('rejectCallHandler()');
 }
