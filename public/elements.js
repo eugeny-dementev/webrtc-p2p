c46109c154280f04044c617bf110412066b735d5
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-16 23:03:21 +0800

    prepare incoming call dialog

--- public/elements.js
+++ public/elements.js
@@ -7,4 +7,57 @@ export function getIncomingCallDialog(callTypeInfo, acceptCallHandler, rejectCal
   assert.isFunction(rejectCallHandler, 'rejectCallHandler should be a function');
 
   console.log('elements.js Getting incoming call');
+
+  const dialog = document.createElement('div');
+  dialog.classList.add('dialog_wrapper');
+
+  const dialogContent = document.createElement('div');
+  dialogContent.classList.add('dialog_content');
+
+  dialog.appendChild(dialogContent);
+
+  const title = document.createElement('p');
+  title.classList.add('dialog_title');
+  title.innerHTML = `Incoming ${callTypeInfo} Call`;
+
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('dialog_image_container');
+  const image = document.createElement('img');
+  image.classList.add('dialog_image');
+  const avatarImagePath = './utils/images/dialogAvatar.png';
+  image.src = avatarImagePath;
+
+  imageContainer.appendChild(image);
+
+  dialogContent.appendChild(title);
+  dialogContent.appendChild(imageContainer);
+
+  const buttonsContainer = document.createElement('div');
+  buttonsContainer.classList.add('dialog_button_container');
+
+  const acceptButton = document.createElement('button');
+  acceptButton.classList.add('dialog_accept_call_button');
+  const acceptCallImage = document.createElement('img');
+  acceptCallImage.classList.add('dialog_button_image');
+  const acceptImagePath = './utils/images/acceptCall.png';
+  acceptCallImage.src = acceptImagePath;
+  acceptButton.appendChild(acceptCallImage);
+  acceptButton.addEventListener('click', acceptCallHandler);
+
+  const rejectButton = document.createElement('button');
+  rejectButton.classList.add('dialog_reject_call_button');
+  const rejectCallImage = document.createElement('img');
+  rejectCallImage.classList.add('dialog_button_image');
+  const rejectImagePath = './utils/images/rejectCall.png';
+  rejectCallImage.src = rejectImagePath;
+  rejectButton.appendChild(rejectCallImage);
+  rejectButton.addEventListener('click', rejectCallHandler);
+
+  buttonsContainer.appendChild(acceptButton);
+  buttonsContainer.appendChild(rejectButton);
+
+  dialogContent.appendChild(buttonsContainer);
+
+  const dialogHTML = document.getElementById('dialog');
+  dialogHTML.appendChild(dialog);
 }
