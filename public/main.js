305d7b6884bab9c45a2f2fe55b1c0671ccd152b7
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-21 21:11:14 +0800

    transform main.js into single function

--- public/main.js
+++ public/main.js
@@ -4,27 +4,29 @@ import * as ui from './ui.js';
 import * as webRTCHandler from './webRTCHandler.js';
 import * as wss from './wss.js';
 
-wss.subscribeToSocketEvent('connect', () => {
-  console.log('success connection to socket.io server with id:', wss.socket.id);
-  store.setSocketId(wss.socket.id);
-  ui.updatePersonalCode(wss.socket.id);
-});
-wss.subscribeToSocketEvent('pre-offer', (data) => {
-  webRTCHandler.handlePreOffer(data)
-});
-wss.subscribeToSocketEvent('pre-offer-answer', (data) => {
-  webRTCHandler.handlePreOfferAnswer(data);
-});
+export function initialize() {
+  wss.subscribeToSocketEvent('connect', () => {
+    console.log('success connection to socket.io server with id:', wss.socket.id);
+    store.setSocketId(wss.socket.id);
+    ui.updatePersonalCode(wss.socket.id);
+  });
+  wss.subscribeToSocketEvent('pre-offer', (data) => {
+    webRTCHandler.handlePreOffer(data)
+  });
+  wss.subscribeToSocketEvent('pre-offer-answer', (data) => {
+    webRTCHandler.handlePreOfferAnswer(data);
+  });
 
-ui.registerCopyCodeButtonHandler();
+  ui.registerCopyCodeButtonHandler();
 
-ui.registerPersonalChatButtonHandler(() => {
-  const code = ui.getCalleePersonalCode();
+  ui.registerPersonalChatButtonHandler(() => {
+    const code = ui.getCalleePersonalCode();
 
-  webRTCHandler.sendPreOffer(code, CALL_TYPE.PersonalChat);
-})
-ui.registerPersonalVideoButtonHandler(() => {
-  const code = ui.getCalleePersonalCode();
+    webRTCHandler.sendPreOffer(code, CALL_TYPE.PersonalChat);
+  })
+  ui.registerPersonalVideoButtonHandler(() => {
+    const code = ui.getCalleePersonalCode();
 
-  webRTCHandler.sendPreOffer(code, CALL_TYPE.PersonalCall);
-});
+    webRTCHandler.sendPreOffer(code, CALL_TYPE.PersonalCall);
+  });
+}
