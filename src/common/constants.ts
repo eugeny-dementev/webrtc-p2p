3c7476a3682b50bafcf0919b6433cf589b3c9091
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-25 21:30:16 +0800

    transform contants to enums

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,21 +1,21 @@
-export const CALL_TYPE = {
-  PersonalCall: 'personal_call',
-  PersonalChat: 'personal_chat',
+export enum CALL_TYPE {
+  PersonalCall = 'personal_call',
+  PersonalChat = 'personal_chat',
 };
 
-export const CALL_INFO = {
-  Chat: 'Chat',
-  Video: 'Video',
+export enum CALL_INFO {
+  Chat = 'Chat',
+  Video = 'Video',
 };
 
-export const CALL_TYPE_TO_INFO = {
+export const CALL_TYPE_TO_INFO: Record<CALL_TYPE, CALL_INFO> = {
   [CALL_TYPE.PersonalCall]: CALL_INFO.Video,
   [CALL_TYPE.PersonalChat]: CALL_INFO.Chat,
 }
 
-export const PRE_OFFER_ANSWER = {
-  CALLEE_NOT_FOUND: 'CALLEE_NOT_FOUND',
-  CALL_ACCEPTED: 'CALL_ACCEPTED',
-  CALL_REJECTED: 'CALL_REJECTED',
-  CALLEE_UNAVAILABLE: 'CALL_UNAVAILABLE',
+export enum PRE_OFFER_ANSWER {
+  CALLEE_NOT_FOUND = 'CALLEE_NOT_FOUND',
+  CALL_ACCEPTED = 'CALL_ACCEPTED',
+  CALL_REJECTED = 'CALL_REJECTED',
+  CALLEE_UNAVAILABLE = 'CALL_UNAVAILABLE',
 };
