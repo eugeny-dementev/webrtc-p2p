b1892ba54d1b8e91754eb1ef3e081e7e7d5e4224
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-25 22:07:45 +0800

    prepare common types file

---
+++ src/common/types.ts
@@ -0,0 +1,26 @@
+import { CALL_TYPE, PRE_OFFER_ANSWER } from "./constants";
+
+export interface CallerPreOffer {
+  callType: CALL_TYPE,
+  calleePersonalCode: string,
+  from: 'front' | 'back',
+  to: 'front' | 'back',
+}
+
+export interface CalleePreOffer {
+  callType: CALL_TYPE,
+  callerSocketId: string,
+  from: 'front' | 'back',
+  to: 'front' | 'back',
+}
+
+export interface CalleePreAnswer {
+  preOfferAnswer: PRE_OFFER_ANSWER,
+  callerSocketId: string,
+  from: 'front' | 'back',
+  to: 'front' | 'back',
+}
+
+export interface CallerPreAnswer {
+}
+
