db58cdaa9b0415ccbad4b32baf2651ce263de8f9
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-26 18:49:54 +0800

    add dialog factory

--- src/front/elements.ts
+++ src/front/elements.ts
@@ -1,62 +1,120 @@
-import { CALL_INFO } from "../common/constants";
+import { injectable } from "inversify";
 import { assert } from "../common/assert";
+import { CALL_INFO } from "../common/constants";
+
+class Dialog {
+  private dialog: HTMLDivElement;
+  private content: HTMLDivElement;
+  private title: HTMLSpanElement;
+  private buttonsContainer?: HTMLDivElement;
+
+  constructor() {
+    this.buildDialog();
+    this.buildContent();
+    this.buildTitle();
+    this.buildImage();
+  }
+
+  private buildDialog() {
+    this.dialog = document.createElement('div');
+    this.dialog.classList.add('dialog_wrapper');
+  }
+
+  private buildContent() {
+    this.content = document.createElement('div');
+    this.content.classList.add('dialog_content');
+
+    this.dialog.appendChild(this.content);
+  }
+
+  private buildTitle() {
+    this.title = document.createElement('p');
+    this.title.classList.add('dialog_title');
+
+    this.content.appendChild(this.title);
+  }
+
+  private buildImage() {
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('dialog_image_container');
+    const image = document.createElement('img');
+    image.classList.add('dialog_image');
+    const avatarImagePath = './utils/images/dialogAvatar.png';
+    image.src = avatarImagePath;
+
+    imageContainer.appendChild(image);
+
+    this.content.appendChild(imageContainer);
+  }
+
+  setTitle(title: string) {
+    this.title.innerHTML = title;
+
+    return this;
+  }
+
+  addButton(type: 'accept' | 'reject', listener: HTMLButtonElement['onclick']) {
+    if (!this.buttonsContainer) {
+      this.buttonsContainer = document.createElement('div');
+      this.buttonsContainer.classList.add('dialog_button_container');
+    }
+
+    const button = document.createElement('button');
+    button.classList.add(`dialog_${type}_call_button`);
+    const callImage = document.createElement('img');
+    callImage.classList.add('dialog_button_image');
+    const imagePath = `./utils/images/${type}Call.png`;
+    callImage.src = imagePath;
+    button.appendChild(callImage);
+    button.addEventListener('click', listener);
+
+    this.buttonsContainer.appendChild(button);
+
+    return this;
+  }
+
+  appendButtons() {
+    assert.isInstanceOf(this.buttonsContainer, HTMLDivElement, 'buttonsContainer must be created before appending');
+    this.content.appendChild(this.buttonsContainer);
+
+    return this;
+  }
+
+  setDescription(descriptionStr: string) {
+    const description = document.createElement('p');
+    description.classList.add('dialog_description');
+    description.innerHTML = descriptionStr;
+
+    this.content.appendChild(description);
+
+    return this;
+  }
+
+  getElement(): HTMLElement {
+    return this.dialog;
+  }
+}
+
+@injectable()
+export class ElementsFactory {
+
+  buildDialogBase(title: string) {
+
+  }
+
+}
 
 export function getIncomingCallDialog(callTypeInfo, acceptCallHandler, rejectCallHandler) {
   assert.oneOf(callTypeInfo, Object.values(CALL_INFO));
   assert.isFunction(acceptCallHandler, 'acceptCallHandler should be a function');
   assert.isFunction(rejectCallHandler, 'rejectCallHandler should be a function');
 
-  console.log('elements.js Getting incoming call');
-
-  const dialog = document.createElement('div');
-  dialog.classList.add('dialog_wrapper');
-
-  const dialogContent = document.createElement('div');
-  dialogContent.classList.add('dialog_content');
-
-  dialog.appendChild(dialogContent);
-
-  const title = document.createElement('p');
-  title.classList.add('dialog_title');
-  title.innerHTML = `Incoming ${callTypeInfo} Call`;
-
-  const imageContainer = document.createElement('div');
-  imageContainer.classList.add('dialog_image_container');
-  const image = document.createElement('img');
-  image.classList.add('dialog_image');
-  const avatarImagePath = './utils/images/dialogAvatar.png';
-  image.src = avatarImagePath;
-
-  imageContainer.appendChild(image);
-
-  dialogContent.appendChild(title);
-  dialogContent.appendChild(imageContainer);
-
-  const buttonsContainer = document.createElement('div');
-  buttonsContainer.classList.add('dialog_button_container');
-
-  const acceptButton = document.createElement('button');
-  acceptButton.classList.add('dialog_accept_call_button');
-  const acceptCallImage = document.createElement('img');
-  acceptCallImage.classList.add('dialog_button_image');
-  const acceptImagePath = './utils/images/acceptCall.png';
-  acceptCallImage.src = acceptImagePath;
-  acceptButton.appendChild(acceptCallImage);
-  acceptButton.addEventListener('click', acceptCallHandler);
-
-  const rejectButton = document.createElement('button');
-  rejectButton.classList.add('dialog_reject_call_button');
-  const rejectCallImage = document.createElement('img');
-  rejectCallImage.classList.add('dialog_button_image');
-  const rejectImagePath = './utils/images/rejectCall.png';
-  rejectCallImage.src = rejectImagePath;
-  rejectButton.appendChild(rejectCallImage);
-  rejectButton.addEventListener('click', rejectCallHandler);
-
-  buttonsContainer.appendChild(acceptButton);
-  buttonsContainer.appendChild(rejectButton);
-
-  dialogContent.appendChild(buttonsContainer);
+  const dialog = new Dialog()
+    .setTitle(`Incoming ${callTypeInfo} Call`)
+    .addButton('accept', acceptCallHandler)
+    .addButton('reject', rejectCallHandler)
+    .appendButtons()
+    .getElement();
 
   return dialog;
 }
