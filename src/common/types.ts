1464afc69a05df7790faf7cbf8c47af4eab9be6c
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-27 19:44:32 +0800

    inject sense to types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,23 +1,31 @@
 import { CALL_TYPE, PRE_OFFER_ANSWER } from "./constants";
 
-export interface CallerPreOffer {
+export interface PreOfferFromCaller {
   callType: CALL_TYPE,
   calleePersonalCode: string,
-  from: 'front' | 'back',
-  to: 'front' | 'back',
+  from: 'front',
+  to: 'back',
 }
-
-export interface CalleePreOffer {
+// caller(PreOfferFromCaller) => back(PreOfferFromCaller->PreOfferForCallee) => callee(PreOfferForCallee)
+export interface PreOfferForCallee {
   callType: CALL_TYPE,
   callerSocketId: string,
-  from: 'front' | 'back',
-  to: 'front' | 'back',
+  from: 'back',
+  to: 'front',
 }
 
-export interface CalleePreAnswer {
+
+export interface PreAnswerFromCallee {
   preOfferAnswer: PRE_OFFER_ANSWER,
   callerSocketId: string,
-  from: 'front' | 'back',
-  to: 'front' | 'back',
+  from: 'front',
+  to: 'back',
+}
+// callee(PreAnswerFromCallee) => back(PreAnswerFromCallee->PreAnswerForCaller) => caller(PreAnswerForCaller)
+export interface PreAnswerForCaller {
+  preOfferAnswer: PRE_OFFER_ANSWER,
+  callerSocketId: string,
+  from: 'back',
+  to: 'front',
 }
 
