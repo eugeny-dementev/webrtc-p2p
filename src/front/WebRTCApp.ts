3a5b8e1be65f7fe19c8198afd5d5edef2bbe31bd
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-27 21:36:25 +0800

    switch to signaling classes in app

--- src/front/WebRTCApp.ts
+++ src/front/WebRTCApp.ts
@@ -2,38 +2,38 @@ import { inject, injectable } from 'inversify';
 import { Socket } from 'socket.io-client';
 import { assert } from '../common/assert';
 import { CALL_TYPE } from '../common/constants';
-import { event } from '../common/helpers';
-import { PreOfferForCallee } from '../common/types';
+import { PreAnswerForCaller, PreOfferForCallee } from '../common/types';
+import { CalleeSignaling } from './CalleeSignaling';
+import { CallerSignaling } from './CallerSignaling';
 import { Store } from './store';
 import { TOKEN } from './tokens';
 import { UI } from './ui';
 import * as webRTCHandler from './webRTCHandler';
-import * as wss from './wss';
 
 @injectable()
 export class WebRTCApp {
   constructor(
     @inject(TOKEN.Store) private readonly store: Store,
     @inject(TOKEN.Socket) private readonly socket: Socket,
+    @inject(TOKEN.CallerSignaling) private readonly callerSignaling: CallerSignaling,
+    @inject(TOKEN.CalleeSignaling) private readonly calleeSignaling: CalleeSignaling,
     @inject(TOKEN.UI) private readonly ui: UI,
   ) { }
 
   async start() {
     this.socket.on('connect', () => {
       console.log('success connection to server using pure Socket with id:', this.socket.id);
-    });
-    const socket = await wss.getSocketConnection();
-    wss.subscribeToSocketEvent(socket, 'connect', () => {
-      console.log('success connection to socket.io server with id:', socket.id);
-      this.store.socketId = socket.id;
+      this.store.socketId = this.socket.id;
       this.ui.updatePersonalCode();
     });
-    wss.subscribeToSocketEvent(socket, event('pre-offer').from('back').to('front'), (data: PreOfferForCallee) => {
-      webRTCHandler.handlePreOffer(data)
-    });
-    wss.subscribeToSocketEvent(socket, event('pre-offer-answer').from('back').to('front'), (data) => {
-      webRTCHandler.handlePreOfferAnswer(data);
-    });
+
+    this.calleeSignaling.subscribeToPreOfferFromCaller((payload: PreOfferForCallee) => {
+      webRTCHandler.handlePreOffer(payload);
+    })
+
+    this.callerSignaling.subscribeToPreAnswerFromCallee((payload: PreAnswerForCaller) => {
+      webRTCHandler.handlePreOfferAnswer(payload);
+    })
 
     this.ui.registerButtonHandler('personal_code_copy_button', () => {
       const code = this.store.socketId;
