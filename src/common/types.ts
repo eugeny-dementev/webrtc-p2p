208950581d147368f0902874ee5067cb34e33c1f
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-27 20:50:01 +0800

    event direction types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,31 +1,33 @@
 import { CALL_TYPE, PRE_OFFER_ANSWER } from "./constants";
 
-export interface PreOfferFromCaller {
+export interface FrontToBack {
+  from: 'front',
+  to: 'back',
+}
+
+export interface BackToFront {
+  from: 'back',
+  to: 'front',
+}
+
+export interface PreOfferFromCaller extends FrontToBack {
   callType: CALL_TYPE,
   calleePersonalCode: string,
-  from: 'front',
-  to: 'back',
 }
 // caller(PreOfferFromCaller) => back(PreOfferFromCaller->PreOfferForCallee) => callee(PreOfferForCallee)
-export interface PreOfferForCallee {
+export interface PreOfferForCallee extends BackToFront {
   callType: CALL_TYPE,
   callerSocketId: string,
-  from: 'back',
-  to: 'front',
 }
 
 
-export interface PreAnswerFromCallee {
+export interface PreAnswerFromCallee extends FrontToBack {
   preOfferAnswer: PRE_OFFER_ANSWER,
   callerSocketId: string,
-  from: 'front',
-  to: 'back',
 }
 // callee(PreAnswerFromCallee) => back(PreAnswerFromCallee->PreAnswerForCaller) => caller(PreAnswerForCaller)
-export interface PreAnswerForCaller {
+export interface PreAnswerForCaller extends BackToFront {
   preOfferAnswer: PRE_OFFER_ANSWER,
   callerSocketId: string,
-  from: 'back',
-  to: 'front',
 }
 
