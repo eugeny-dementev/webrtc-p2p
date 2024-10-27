357cad0f6cdd8f701d27711918d2571e7f7ebe75
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-27 22:36:09 +0800

    update how preoffer is handled

--- src/front/CalleeEventsHandler.ts
+++ src/front/CalleeEventsHandler.ts
@@ -1,6 +1,9 @@
 import { inject, injectable } from "inversify";
+import { assert } from "../common/assert";
+import { CALL_TYPE, PreOfferAnswer } from "../common/constants";
 import { PreOfferForCallee } from "../common/types";
 import { CalleeSignaling } from "./CalleeSignaling";
+import { Store } from "./store";
 import { TOKEN } from "./tokens";
 import { UI } from "./ui";
 
@@ -8,6 +11,7 @@ import { UI } from "./ui";
 export class CalleeEventsHandler {
   constructor(
     @inject(TOKEN.CalleeSignaling) private readonly callee: CalleeSignaling,
+    @inject(TOKEN.Store) private readonly store: Store,
     @inject(TOKEN.UI) private readonly ui: UI,
   ) { }
 
@@ -17,5 +21,25 @@ export class CalleeEventsHandler {
     })
   }
 
-  handlePreOffer(payload: PreOfferForCallee) { }
+  handlePreOffer(payload: PreOfferForCallee) {
+    assert.oneOf(payload.callType, Object.values(CALL_TYPE));
+    assert.isString(payload.callerSocketId, 'data.callerSocketId should be a string');
+
+    const { callType, callerSocketId } = payload;
+
+    this.store.callerSocketId = callerSocketId;
+
+    if (callType === CALL_TYPE.PersonalChat || callType === CALL_TYPE.PersonalCall) {
+      this.ui.showIncomingCallingDialog(
+        callType,
+        () => {
+          this.callee.emitPreAnswerToCaller(PreOfferAnswer.CallAccepted);
+          this.ui.showCallElements(payload.callType);
+        },
+        () => {
+          this.callee.emitPreAnswerToCaller(PreOfferAnswer.CallRejected);
+        },
+      );
+    }
+  }
 }
