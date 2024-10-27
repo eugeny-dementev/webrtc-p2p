3f6e8ab2db7fad6add20d210fd91d25bd704d3a8
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-27 17:56:37 +0800

    add inverted promise

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -4,3 +4,24 @@ export const event = (name: string) => ({
     to: (target: string) => `event(${name}):from(${source}):to(${target})`,
   }),
 });
+
+type InvertedPromise<T> = {
+  promise: Promise<T>,
+  resolve: (value: T | PromiseLike<T>) => void,
+  reject: (reason?: any) => void,
+}
+export function getInvertedPromise<T>(): InvertedPromise<T> {
+  let resolve: (value: T | PromiseLike<T>) => void;
+  let reject: <T = never>(reason?: any) => void;
+
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  })
+
+  return {
+    promise,
+    resolve,
+    reject,
+  }
+}
