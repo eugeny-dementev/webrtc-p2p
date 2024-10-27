0c9170102a9191eedc492300875c80c294c95844
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-27 21:14:27 +0800

    save caller id to state

--- src/front/CalleeSignaling.ts
+++ src/front/CalleeSignaling.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "inversify";
 import { Socket } from "socket.io-client";
+import { assert } from "../common/assert";
 import { frontToBack, PRE_OFFER_ANSWER, SIGNALING_EVENT } from "../common/constants";
 import { PreAnswerFromCallee, PreOfferForCallee } from "../common/types";
 import { Store } from "./store";
@@ -13,7 +14,13 @@ export class CalleeSignaling {
   ) { }
 
   onPreOfferFromCaller(callback: (payload: PreOfferForCallee) => void) {
-    this.socket.on(SIGNALING_EVENT.PRE_OFFER_FOR_CALLEE, callback);
+    this.socket.on(SIGNALING_EVENT.PRE_OFFER_FOR_CALLEE, (payload: PreOfferForCallee) => {
+      assert.isFalse(payload.callerSocketId === this.store.socketId, 'PreOffer should never came from the same socket id');
+
+      this.store.callerSocketId = payload.callerSocketId;
+
+      return callback(payload);
+    });
   }
   emitPreAnswerToCaller(preOfferAnswer: PRE_OFFER_ANSWER) {
     const payload: PreAnswerFromCallee = {
