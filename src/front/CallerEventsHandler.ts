bc9f8fdbb2cb546bdd83e04e2fba5397480f4646
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-28 22:28:21 +0800

    implement caller handler

--- src/front/CallerEventsHandler.ts
+++ src/front/CallerEventsHandler.ts
@@ -1,6 +1,9 @@
 import { inject, injectable } from "inversify";
+import { assert } from "../common/assert";
+import { PreOfferAnswer } from "../common/constants";
 import { PreAnswerForCaller } from "../common/types";
 import { CallerSignaling } from "./CallerSignaling";
+import { Store } from "./store";
 import { TOKEN } from "./tokens";
 import { UI } from "./ui";
 
@@ -8,6 +11,7 @@ import { UI } from "./ui";
 export class CalleeEventsHandler {
   constructor(
     @inject(TOKEN.CallerSignaling) private readonly caller: CallerSignaling,
+    @inject(TOKEN.Store) private readonly store: Store,
     @inject(TOKEN.UI) private readonly ui: UI,
   ) { }
 
@@ -17,6 +21,28 @@ export class CalleeEventsHandler {
     })
   }
 
-  handlePreAnswer(payload: PreAnswerForCaller) { }
+  handlePreAnswer(payload: PreAnswerForCaller) {
+    const { preOfferAnswer } = payload;
+
+    assert.oneOf(preOfferAnswer, Object.values(PreOfferAnswer));
+
+    switch (preOfferAnswer) {
+      case PreOfferAnswer.CalleeUnavailable:
+      case PreOfferAnswer.CalleeNotFound:
+      case PreOfferAnswer.CallRejected: {
+        this.ui.showInfoDialog(preOfferAnswer);
+        break;
+      }
+      case PreOfferAnswer.CallAccepted: {
+        this.ui.removeDialog();
+        this.ui.showCallElements(this.store.callType);
+        break;
+      };
+      default: {
+        this.ui.removeDialog();
+        throw new TypeError('Improsible pre offer answer value' + preOfferAnswer);
+      }
+    }
+  }
 }
 
